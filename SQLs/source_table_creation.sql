87371ad096558a2787a397eaea1d71748de3105d
rajkaste <rajkaste@gmail.com>
2025-04-27 14:21:08 +0530

    SQL folder added

---
+++ SQLs/source_table_creation.sql
@@ -0,0 +1,15 @@
+CREATE TABLE source_cars_data
+(
+	Branch_ID Varchar(200),
+	Dealer_ID Varchar(200),
+	Model_ID Varchar(200),
+	Revenue BIGINT,
+	Units_Sold BIGINT,
+	Date_ID Varchar(200),
+	Day INT,
+	Month Varchar(200),	
+	Year INT,
+	BranchName Varchar(200),
+	DealerName Varchar(200),
+	Product_Name Varchar(200)
+)
