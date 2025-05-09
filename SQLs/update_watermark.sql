3c7736536ae47a7760109265900afa9425cc69f2
rajkaste <rajkaste@gmail.com>
2025-05-09 19:29:34 +0530

    stored procedure updates

--- SQLs/update_watermark.sql
+++ SQLs/update_watermark.sql
@@ -6,7 +6,7 @@ BEGIN
     BEGIN TRANSACTION;
 
     -- Update the incremental column in the watermark table
-        UPDATE watermark_table
+        UPDATE water_table
         SET last_load = @lastload
 
     COMMIT TRANSACTION;
